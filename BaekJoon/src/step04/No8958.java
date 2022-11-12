05dc2c3342432f8344f993092a6c6934fa2f5c05
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-12 17:58:16 +0900

    백준 알고리즘 - 4. 1차원 배열 - 8958 OX퀴즈

--- BaekJoon/src/step04/No8958.java
+++ BaekJoon/src/step04/No8958.java
@@ -1,28 +1,30 @@
 package step04;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class No8958 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int c = sc.nextInt();
-        String[] quiz = new String[c];
-        int score = 0;
-
-        for (int i = 0; i < quiz.length; i++) {
-            quiz[i] = sc.next();
-        }
-
-        System.out.println(Arrays.toString(quiz));
-
-        for (String q : quiz) {
-            for (int i = 0; i < q.length(); i++) {
-                if (q.charAt(i) == 'O' && q.charAt(i) == q.charAt(i + 1)) {
-                    score = score + 1;
+        int C = sc.nextInt();
+        for (int i = 0; i < C; i++) {
+            String str = sc.next();
+            int[] scores = new int[str.length()];
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == 'O') {
+                    count++;
+                    scores[j] = count;
+                } else {
+                    count = 0;
                 }
             }
+
+            int sum = 0;
+            for (int score : scores) {
+                sum += score;
+            }
+            System.out.println(sum);
         }
     }
 }
