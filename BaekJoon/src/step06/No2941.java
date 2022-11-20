e5b27de950f2f275beca003d87a7304e8ab5fac1
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-11-20 22:06:07 +0900

    백준 알고리즘 - 6. 문자열 2941 크로아티아 알파벳

--- BaekJoon/src/step06/No2941.java
+++ BaekJoon/src/step06/No2941.java
@@ -1,28 +1,18 @@
 package step06;
 
-import java.util.Objects;
+import java.util.Scanner;
 
 public class No2941 {
     public static void main(String[] args) {
-        String str = "ljes=njak";
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
         String[] strArr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 
-        String newStr = "";
-        for (int i = 0; i < strArr.length; i++) {
-            if (str.contains(strArr[i])) {
-                newStr += str.replace(strArr[i], "0");
-                strArr[i] = "0";
+        for (String alphabet : strArr) {
+            if (str.contains(alphabet)) {
+                str = str.replace(alphabet, "0");
             }
         }
-
-        System.out.println(newStr);
-
-        int count = 0;
-        for (String s : strArr) {
-            if (Objects.equals(s, "0")) {
-                count++;
-            }
-        }
-        System.out.println(count);
+        System.out.println(str.length());
     }
 }
