466f894d959a2eb2dc8756b315c329ba034214a5
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-15 22:37:30 +0900

    프로그래머스 > 연습문제 > 정수 제곱근 판별

---
+++ Programmers/src/practice/No12934.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class No12934 {
+
+    public long solution(long n) {
+        long answer = 0;
+        double sqrt = Math.sqrt(n);
+        long flag = (long) sqrt;
+
+        if (sqrt == flag) {
+            answer = (flag + 1) * (flag + 1);
+        } else {
+            answer = -1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        No12934 no12934 = new No12934();
+        long solution1 = no12934.solution(121);
+        System.out.println(solution1);
+        long solution2 = no12934.solution(3);
+        System.out.println(solution2);
+    }
+}
