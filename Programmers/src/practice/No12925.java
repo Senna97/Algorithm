605b2cce26f66bc615e4c4f9c2aa7806996aec24
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2022-12-15 22:36:01 +0900

    프로그래머스 > 연습문제 > 문자열을 정수로 바꾸기

---
+++ Programmers/src/practice/No12925.java
@@ -0,0 +1,26 @@
+package practice;
+
+public class No12925 {
+
+    public int solution(String s) {
+        int answer = 0;
+
+        if (s.charAt(0) == '-') {
+            s = s.substring(1);
+            answer = Integer.parseInt(s) * -1;
+        } else {
+            answer = Integer.parseInt(s);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        No12925 no12925 = new No12925();
+        int solution1 = no12925.solution("+1234");
+        System.out.println(solution1);
+        int solution2 = no12925.solution("-1234");
+        System.out.println(solution2);
+    }
+}
