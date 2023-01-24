fd21a877c87e529e5c1e4c44ccc82093cce05c64
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-01-24 15:09:33 +0900

    프로그래머스 > 연습문제 > 삼총사

---
+++ Programmers/src/practice/No131705.java
@@ -0,0 +1,20 @@
+package practice;
+
+// 프로그래머스 > 연습문제 > 삼총사
+public class No131705 {
+
+    public int solution(int[] number) {
+
+        int answer = 0;
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        No131705 no131705 = new No131705();
+        int solution = no131705.solution(new int[]{-2, 3, 0, 2, -5});
+        System.out.println(solution);
+    }
+}
