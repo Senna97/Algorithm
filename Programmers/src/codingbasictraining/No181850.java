e5f56f7ef99012191d05f0920fb24ba02cc169a2
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-04-24 21:23:33 +0900

    프로그래머스 > 코딩 기초 트레이닝 > 정수 부분

---
+++ Programmers/src/codingbasictraining/No181850.java
@@ -0,0 +1,15 @@
+package codingbasictraining;
+
+// 프로그래머스 > 코딩 기초 트레이닝 > 정수 부분
+public class No181850 {
+
+    public int solution(double flo) {
+        return (int) flo;
+    }
+
+    public static void main(String[] args) {
+        No181850 no181850 = new No181850();
+        int solution = no181850.solution(1.42);
+        System.out.println(solution);
+    }
+}
