ec2102f16fe9db9e60586a15a8f52b8005ec5626
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-04-25 20:43:49 +0900

    프로그래머스 > 코딩 기초 트레이닝 > 문자열로 변환

---
+++ Programmers/src/codingbasictraining/No181845.java
@@ -0,0 +1,15 @@
+package codingbasictraining;
+
+// 프로그래머스 > 코딩 기초 트레이닝 > 문자열로 변환
+public class No181845 {
+
+    public String solution(int n) {
+        return String.valueOf(n);
+    }
+
+    public static void main(String[] args) {
+        No181845 no181845 = new No181845();
+        String solution = no181845.solution(123);
+        System.out.println(solution);
+    }
+}
