102566e0b920d752930fb913ae941d20e8000be2
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-05-24 08:51:45 +0900

    프로그래머스 > 코딩 기초 트레이닝 > 문자열 붙여서 출력하기

---
+++ Programmers/src/codingbasictraining/No181946.java
@@ -0,0 +1,15 @@
+package codingbasictraining;
+
+import java.util.Scanner;
+
+// 프로그래머스 > 코딩 기초 트레이닝 > 문자열 붙여서 출력하기
+public class No181946 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a + "" + b);
+    }
+}
