a67ca0ae0e054b8c26ecbcef598232208b19d651
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-09-09 12:38:04 +0900

    프로그래머스 > 코딩 기초 트레이닝 > a와 b 출력하기

---
+++ Programmers/src/codingbasictraining/No181951.java
@@ -0,0 +1,17 @@
+package codingbasictraining;
+
+import java.util.Scanner;
+
+// 프로그래머스 > 코딩 기초 트레이닝 > a와 b 출력하기
+public class No181951 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
