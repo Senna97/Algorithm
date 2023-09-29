f71597dcb865ddf9d660dfb41fd4773d41db188e
Senna97 <wkdtjgus0319@skuniv.ac.kr>
2023-09-29 18:37:14 +0900

    프로그래머스 > 코딩 기초 트레이닝 > 문자열 출력하기

---
+++ Programmers/src/codingbasictraining/No181952.java
@@ -0,0 +1,15 @@
+package codingbasictraining;
+
+import java.util.Scanner;
+
+// 프로그래머스 > 코딩 기초 트레이닝 > 문자열 출력하기
+public class No181952 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+
+}
